6eb1a55c6b5f7e9884fc6b4a9260379172f4183c
Andrei Ionescu <voidwarden@yahoo.com>
2021-12-02 19:16:46 +0200

    Add total functionality

--- static/js/content/products/productFilters.client.js
+++ static/js/content/products/productFilters.client.js
@@ -1,26 +1,157 @@
 window.addEventListener('DOMContentLoaded', (event) => {
-    applyActiveFilters();
+    addActiveFilters();
+    addResetFilters();
+    addTotalFilter();
+    addSortFilter();
 });
+
+function addSortFilter() {
+    
+}
+
+function addTotalFilter() {
+    const totalButton = document.getElementById('total-button');
+    let currentTimeout = null;
+
+    totalButton.addEventListener('click', function(event) {
+        const visibleProducts = Array.from(getProductElements())
+            .filter(p => p.style.display !== 'none');
+
+        const total = visibleProducts
+            .map(p => {
+                let trunc = p.getElementsByClassName('price-value-trunc')[0]
+                    .innerHTML.trim();
+                let decim = p.getElementsByClassName('price-value-decim')[0]
+                    .innerHTML.trim();
+                trunc = parseInt(trunc);
+                decim = parseInt(decim);
+                const price = trunc + decim / 100;
+                return price;
+            })
+            .reduce((x, y) => x + y);
+
+        const actionElements = document.getElementsByClassName('product-actions')[0];
+        let totalElement = document.getElementById('total-product-sum');
+
+        if (!totalElement) {
+            totalElement = document.createElement('div');
+            actionElements.appendChild(totalElement);
+            totalElement.id = 'total-product-sum';
+        }
+
+        totalElement.innerHTML = `Total Sum: ${total.toFixed(2)}`;
+
+        if (currentTimeout) {
+            clearTimeout(currentTimeout);
+        }
+
+        currentTimeout = setTimeout(() => {
+            actionElements.removeChild(totalElement);
+            currentTimeout = null;
+        }, 2000);
+    });
+}
+
+function addResetFilters() {
+    const resetFiltersButton = document.getElementById('reset-filters-button');
+    resetFiltersButton.addEventListener('click', () => applyResetFilters());
+}
+
+function applyResetFilters() {
+    const isValid = validateAllFilters();
+
+    if (!isValid) {
+        return;
+    }
+
+    const resets = [
+        resetProductName,
+        resetHasAudioJack,
+        resetPrice,
+        resetOperatingSystem,
+        resetCategories,
+        resetTechnologies,
+        resetReleaseYear,
+        resetBatteryCapacity,
+    ];
+
+    for (const reset of resets) {
+        reset();
+    }
+
+    applyActiveFilters();
+}
+
+function resetProductName() {
+    const productNameInput = document.getElementById('name-input');
+    productNameInput.value = '';
+}
+
+function resetHasAudioJack() {
+    const hasAudioJackInput = document.getElementById('has-jack-input');
+    hasAudioJackInput.checked = false;
+}
+
+function resetPrice() {
+    const priceInput = document.getElementById('price-input');
+    priceInput.value = priceInput.max;
+    const currentPrice = document.getElementById('price-current');
+    currentPrice?.remove();
+}
+
+function resetOperatingSystem() {
+    const osInput = document.querySelectorAll('input[name="os-input"]');
+    osInput.forEach(os => os.checked = false);
+    const osInputDefault = document.getElementById('os-default-input');
+    osInputDefault.checked = true;
+}
+
+function resetCategories() {
+    const categoriesInput = document.querySelectorAll('input[name="category-input"]');
+    categoriesInput.forEach(category => category.checked = true);
+}
+
+function resetTechnologies() {
+    const technologiesInput = document.getElementById('technologies-input');
+    technologiesInput.value = "";
+}
+
+function resetReleaseYear() {
+    const yearsInput = Array
+        .from(document.querySelectorAll('#release-year-input > option'));
+    yearsInput.forEach(year => year.selected = false);
+    yearsInput.filter(year => year.value === "default")
+        .forEach(year => year.selected = true);
+}
+
+function resetBatteryCapacity() {
+    const batteryInput = Array
+        .from(document.querySelectorAll('#battery-input > option'));
+    batteryInput.forEach(battery => battery.selected = false);
+    batteryInput.filter(battery => battery.value === "default")
+        .forEach(battery => battery.selected = true);
+}
+
+function addActiveFilters() {
+    const applyFiltersButton = document.getElementById('apply-filters-button');
+    applyFiltersButton.addEventListener('click', () => applyActiveFilters());
+}
 
 function applyActiveFilters() {
     const products = getProductElements();
-    const applyFiltersButton = document.getElementById('apply-filters-button');
+    const isValid = validateAllFilters();
 
-    applyFiltersButton.addEventListener('click', function(event) {
-        const isValid = validateAllFilters();
+    if (!isValid) {
+        return;
+    }
 
-        if (!isValid) {
-            return;
+    for (const product of products) {
+        if (isProductIncluded(product)) {
+            product.style.display = 'flex';
+        } else {
+            product.style.display = 'none';
         }
-
-        for (const product of products) {
-            if (isProductIncluded(product)) {
-                product.style.display = 'flex';
-            } else {
-                product.style.display = 'none';
-            }
-        }
-    });
+    }
 }
 
 function isProductIncluded(product) {
@@ -53,7 +184,7 @@ function isProductNameIncluded(product) {
 
     const nameValue = product.getElementsByClassName('name-value')[0];
     const processedValue = nameValue.innerHTML.trim().toLowerCase();
-    return processedValue.includes(nameInput.value)
+    return processedValue.includes(nameInput.value.toLowerCase())
 }
 
 function isAudioJackIncluded(product) {
