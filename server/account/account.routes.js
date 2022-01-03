ee6d5e34aa36d6910469ea4d59d8b6bbd76aebd7
Andrei Ionescu <voidwarden@yahoo.com>
2022-01-03 21:08:15 +0200

    Add raw form

--- server/account/account.routes.js
+++ server/account/account.routes.js
@@ -2,11 +2,13 @@ function init({ accountService, productsService }) {
   const express = require("express");
   const router = express.Router();
 
-  router.get("", (req, res, next) => {
+  router.get('', (req, res, next) => {
     res.render(`pages/content/account/account`);
   });
 
-  router.get("/register", (req, res, next) => {});
+  router.get('/register', (req, res, next) => {
+    res.render(`pages/content/account/register`);
+  });
 
   return router;
 }
